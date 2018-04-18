daa8c33f7a34215a42013564a2ba9d963483de8f
NGUYEN Nhat Minh <nath-minh.nguyen_externe@group-sma.fr>
2018-04-18 17:20:36 +0200

    broadcast event

--- index.js
+++ index.js
@@ -8,8 +8,14 @@ var io = socketio(server);
 app.use(express.static('static'));
 
 io.on('connection', (socket) => {
+  /**
+   * Khi một thag join, gọi là thag A đi, thì nó sẽ broadcast cho tụi còn lại
+   * B C D gì đó là nó join 
+   */
+   
+  socket.broadcast.emit('user.events', 'Someone has joined!');
   socket.on('name', (name) => {
     console.log(name + ' says hello!');
-    io.emit('name', name);
+    socket.broadcast.emit('name', name);
   });
 });
