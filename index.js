c3bd41bfc5a3a567641928b982ce8ea05e54a8ba
NGUYEN Nhat Minh <nath-minh.nguyen_externe@group-sma.fr>
2018-04-19 11:36:01 +0200

    connect express and socket io using express-session middleware

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
 const express = require('express'),
+  session = require('express-session'),
   socketio = require('socket.io');
 
 var app = express();
 var server = app.listen(8080);
 var io = socketio(server);
 
+var sessionMiddleware = session({
+  secret: 'make this a good secret',
+  resave: false,
+  saveUninitialized: true
+});
+
+//setup express to use middleware
+app.use(sessionMiddleware);
+app.use((req, res, next) => {
+  console.log(`From Express: ${req.session.name}`);
+  next();
+});
 app.use(express.static('static'));
 
-var namespaceHandler = (namespace) => {
-  return (socket) => {
-    socket.emit('event', 'You joined ' + namespace.name);
-    //just resend it
-    socket.on('event', (data) => {
-      socket.broadcast.emit('event', data);
-    });
-  };
-}
+//socket.io middleware
+io.use((socket, next) => {sessionMiddleware(socket.request, {}, next);});
 
-var one = io.of('/namespace1');
-var two = io.of('/namespace2');
+// Mục đích của chương này là lưu socket vào session của express bằng 
+// middleware express-session
 
-one.on('connection', namespaceHandler(one));
-two.on('connection', namespaceHandler(two));
+io.on('connection', (socket) => {
+  console.log(socket.request.session);
+  // Tìm trong session của express server nếu chưa thấy name dc lưu vào
+  
+  if(socket.request.session.name !== undefined){
+    socket.emit('name', socket.request.session.name);
+    io.emit('event', socket.request.session.name + ' has joined!');
+  }
+
+
+  socket.on('name', (name) => {
+    socket.request.session.name = name;
+    socket.request.session.save();
+    socket.broadcast.emit('event', name + ' says hello!');
+  });
+});
