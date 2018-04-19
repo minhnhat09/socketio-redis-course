b283d11ac60ec6e5359a9398c887d5b7def7b2fe
NGUYEN Nhat Minh <nath-minh.nguyen_externe@group-sma.fr>
2018-04-19 11:40:33 +0200

    Introduction to express routes

--- index.js
+++ index.js
@@ -1,44 +1,16 @@
 const express = require('express'),
-  session = require('express-session'),
-  socketio = require('socket.io');
+  socketio = require('socket.io'),
+  router = require('./routes.js');
 
 var app = express();
 var server = app.listen(8080);
 var io = socketio(server);
 
-var sessionMiddleware = session({
-  secret: 'make this a good secret',
-  resave: false,
-  saveUninitialized: true
-});
 
 //setup express to use middleware
-app.use(sessionMiddleware);
-app.use((req, res, next) => {
-  console.log(`From Express: ${req.session.name}`);
-  next();
-});
-app.use(express.static('static'));
-
-//socket.io middleware
-io.use((socket, next) => {sessionMiddleware(socket.request, {}, next);});
-
-// Mục đích của chương này là lưu socket vào session của express bằng 
-// middleware express-session
+app.use('/', router);
 
 io.on('connection', (socket) => {
-  console.log(socket.request.session);
-  // Tìm trong session của express server nếu chưa thấy name dc lưu vào
-  
-  if(socket.request.session.name !== undefined){
-    socket.emit('name', socket.request.session.name);
-    io.emit('event', socket.request.session.name + ' has joined!');
-  }
-
-
-  socket.on('name', (name) => {
-    socket.request.session.name = name;
-    socket.request.session.save();
-    socket.broadcast.emit('event', name + ' says hello!');
-  });
+  //we need specific events for each
+  //cubs and bears
 });
