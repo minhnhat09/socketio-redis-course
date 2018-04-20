5a3ce2c9b2975d31ae64332bde7feca761554d38
NGUYEN Nhat Minh <nath-minh.nguyen_externe@group-sma.fr>
2018-04-20 11:14:37 +0200

    Using Redis for multiple server Socket.IO

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 module.exports = {
   redis_host: '127.0.0.1',
-  redis_port: 16379,
-  expire: 60 * 60 * 1000
+  redis_port: 16379
 }
--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 const express = require('express'),
   socketio = require('socket.io'),
-  router = require('./routes.js'),
-  sockets = require('./sockets.js');
+  process = require('process'),
+  config = require('./config.js'),
+  socketioRedis = require('socket.io-redis');
 
 var app = express();
-var server = app.listen(8080);
+var server = app.listen(process.argv[2]);
 var io = socketio(server);
 
+app.use(express.static('static'));
 
-//setup express to use middleware
-app.use('/', router);
-
-var bears = io.of('/bears');
-var cubs = io.of('/cubs');
-bears.on('connection', sockets.bearsNamespace);
-cubs.on('connection', sockets.cubsNamespace);
-
+io.adapter(socketioRedis({host: config.redis_host, port: config.redis_port}));
 io.on('connection', (socket) => {
-  //we need specific events for each
-  //cubs and bears
+  socket.on('room.join', (room) => {
+    console.log(socket.rooms);
+    Object.keys(socket.rooms).filter((r) => r != socket.id)
+    .forEach((r) => socket.leave(r));
+
+    setTimeout(() => {
+      socket.join(room);
+      socket.emit('event', 'Joined room ' + room);
+      socket.broadcast.to(room).emit('event', 'Someone joined room ' + room);
+    }, 0);
+  })
+
+  socket.on('event', (e) => {
+    socket.broadcast.to(e.room).emit('event', e.name + ' says hello!');
+  });
+
 });
